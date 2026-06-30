a4972e95d50b8fca8e5272038b2f8fe9be379e47
kzz54tzg5r-code <kzz54tzg5r@privaterelay.appleid.com>
2026-06-30 12:06:57 -0600

    Delete 4_Recorridos.py

--- 4_Recorridos.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import streamlit as st
-from modules.ui import setup_page, section_title
-
-setup_page("ORION | Panel_Ejecutivo")
-section_title("Panel Ejecutivo")
-st.info("Esta sección está preparada para integrar los indicadores específicos del proyecto ORION.")
