30f1be98ea8ccb0259c7f702082e65c7481733cd
kzz54tzg5r-code <kzz54tzg5r@privaterelay.appleid.com>
2026-06-30 12:09:07 -0600

    Delete 8_Configuracion.py

--- 8_Configuracion.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import streamlit as st
-from modules.ui import setup_page, section_title
-
-setup_page("ORION | Rankings")
-section_title("Rankings")
-st.info("Esta sección está preparada para integrar los indicadores específicos del proyecto ORION.")
