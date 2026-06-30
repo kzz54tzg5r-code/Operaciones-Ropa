1c65e84c9a4a69809d5b958f363f000b4d6bc6b2
kzz54tzg5r-code <kzz54tzg5r@privaterelay.appleid.com>
2026-06-30 12:10:41 -0600

    Delete loader.py

--- loader.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import streamlit as st
-from modules.ui import setup_page, section_title
-
-setup_page("ORION | Por_Dia")
-section_title("Por Dia")
-st.info("Esta sección está preparada para integrar los indicadores específicos del proyecto ORION.")
