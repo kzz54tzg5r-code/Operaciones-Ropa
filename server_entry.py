17f08ce56f01a135173639c22b7e7ce25a4e04a1
kzz54tzg5r-code <albertoangelhz@icloud.com>
2026-09-21 10:11:20 -0600

    V177 prepara alta concurrencia con cache y rollback

--- server_entry.py
+++ server_entry.py
@@ -77,6 +77,7 @@ from v172_density_balance_patch import install as _install_v172_density_balance_
 from v174_mobile_sales_repair_patch import install as _install_v174_mobile_sales_repair_patch
 from v175_compact_filter_tabs_patch import install as _install_v175_compact_filter_tabs_patch
 from v176_commercial_integral_patch import install as _install_v176_commercial_integral_patch
+from v177_scale_800_foundation_patch import install as _install_v177_scale_800_foundation_patch
 
 _v121_operation_module.Request = _FastAPIRequest
 
@@ -150,4 +151,5 @@ _install_v172_density_balance_patch(web_app)
 _install_v174_mobile_sales_repair_patch(web_app)
 _install_v175_compact_filter_tabs_patch(web_app)
 _install_v176_commercial_integral_patch(web_app)
+_install_v177_scale_800_foundation_patch(web_app)
 app = web_app.app
